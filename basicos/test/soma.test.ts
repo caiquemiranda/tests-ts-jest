6045cbfbf13f68cec2fe7f873404acfd34047d92
caiquemiranda <caiquemirandas@gmail.com>
2025-07-01 19:42:17 -0300

    using function it too

--- basicos/test/soma.test.ts
+++ basicos/test/soma.test.ts
@@ -12,3 +12,7 @@ test("Deve testar a soma com numeros positivos", ()=>{
 test("Deve testar a soma com numeros negativos", ()=>{
     expect(soma(-1,-2)).toBe(-3)
 })
+
+it("should test the function with it", ()=>{
+    expect(soma(1,2)).toBe(3)
+})
