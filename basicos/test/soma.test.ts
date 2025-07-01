e811997e949a242fae22670d598fe6e1a1a26e98
caiquemiranda <caiquemirandas@gmail.com>
2025-07-01 19:39:48 -0300

    specificity test

--- basicos/test/soma.test.ts
+++ basicos/test/soma.test.ts
@@ -4,3 +4,11 @@ test("Deve testar a funcao soma", ()=>{
     const resultado = soma(1,2)
     expect(resultado).toBe(3)
 })
+
+test("Deve testar a soma com numeros positivos", ()=>{
+    expect(soma(1,2)).toBe(3)
+})
+
+test("Deve testar a soma com numeros negativos", ()=>{
+    expect(soma(-1,-2)).toBe(-3)
+})
